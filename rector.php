0829ea30712135cf74e17729f2ce06a93571961b
Nicolas Kion <nicolaskion07@gmail.com>
2026-08-10 15:17:19 +0200

    chore: update all dependencies and drop deprecated rector rules

--- rector.php
+++ rector.php
@@ -6,17 +6,13 @@ use Rector\CodeQuality\Rector\BooleanAnd\SimplifyEmptyArrayCheckRector;
 use Rector\CodeQuality\Rector\Catch_\ThrowWithPreviousExceptionRector;
 use Rector\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector;
 use Rector\CodeQuality\Rector\Expression\TernaryFalseExpressionToIfRector;
-use Rector\CodeQuality\Rector\FuncCall\SimplifyRegexPatternRector;
 use Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector;
 use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 use Rector\CodeQuality\Rector\Identical\SimplifyConditionsRector;
 use Rector\CodeQuality\Rector\Identical\StrlenZeroToIdenticalEmptyStringRector;
 use Rector\CodeQuality\Rector\NullsafeMethodCall\CleanupUnneededNullsafeOperatorRector;
-use Rector\CodeQuality\Rector\Switch_\SwitchTrueToIfRector;
 use Rector\CodeQuality\Rector\Ternary\SwitchNegatedTernaryRector;
 use Rector\CodeQuality\Rector\Ternary\UnnecessaryTernaryExpressionRector;
-use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
-use Rector\CodingStyle\Rector\FuncCall\CountArrayToEmptyArrayComparisonRector;
 use Rector\CodingStyle\Rector\String_\SimplifyQuoteEscapeRector;
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\If_\ReduceAlwaysFalseIfOrRector;
@@ -37,17 +33,13 @@ return RectorConfig::configure()
             SimplifyConditionsRector::class,
             SimplifyEmptyArrayCheckRector::class,
             SimplifyEmptyCheckOnEmptyArrayRector::class,
-            SimplifyRegexPatternRector::class,
             SimplifyUselessVariableRector::class,
             StrlenZeroToIdenticalEmptyStringRector::class,
             SwitchNegatedTernaryRector::class,
-            SwitchTrueToIfRector::class,
             TernaryFalseExpressionToIfRector::class,
             TernaryToNullCoalescingRector::class,
             ThrowWithPreviousExceptionRector::class,
             UnnecessaryTernaryExpressionRector::class,
-            CountArrayToEmptyArrayComparisonRector::class,
-            EncapsedStringsToSprintfRector::class,
             SimplifyQuoteEscapeRector::class,
             ReduceAlwaysFalseIfOrRector::class,
             RemoveAlwaysTrueIfConditionRector::class,
